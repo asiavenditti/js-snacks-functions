3b14e3d646dfbf5a1aa4c404154dcfa70b3662f2
Your Name <you@example.com>
2025-06-04 18:17:48 +0200

    Implement function to count vowels in a string and log the result to console

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,32 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+// 'javascript'
+function countVowels(text) {
+
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+
+    let vowelsOnly = []
+    for (let i = 0; i < text.length; i++) {
+
+        const letter = text[i]
+
+
+
+        if (vowels.includes(letter)) {
+            vowelsOnly.push(letter)
+        }
+
+
+    }
+
+    return vowelsOnly.length
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
-
+const numberOfVowels = countVowels(word)
+console.log(numberOfVowels)
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
