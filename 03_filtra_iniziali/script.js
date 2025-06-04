2850abc85ab3b415a9abdc01e05ceb0fb4e72212
Your Name <you@example.com>
2025-06-04 15:47:12 +0200

    Implement function to filter names by initial letter and log results to console

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,27 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function nameSelection(names_list, letter) {
+
+    let namesWithLetter = []
+
+    for (let i = 0; i < names_list.length; i++) {
+        const singleName = names_list[i];
+        if (singleName[0] === letter) {
+
+            namesWithLetter.push(singleName);
+
+        }
+
+    }
+
+    return namesWithLetter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const only_with_a = nameSelection(names, 'A');
+console.log(only_with_a);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
