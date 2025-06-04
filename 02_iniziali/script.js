8d16cdd2a501c93440caa39b8baf9b08b8d48012
Your Name <you@example.com>
2025-06-04 15:03:53 +0200

    Implement the function to get the initials from an array of names and print the result to the console

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function getFirstLetter(names) {
+
+    let firstLetters = [];
+
+    for (let i = 0; i < names.length; i++) {
+        let singleName = names[i];
+        const initialLetters = singleName.charAt(0);
+        firstLetters.push(initialLetters);
+
+
+    }
+    return firstLetters;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const initialList = getFirstLetter(names);
+console.log(initialList);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
